e6bdc4456a13cf7699403e8a317fe4192d2f0201
Worrameth Manajit <40156085+Worrameth@users.noreply.github.com>
2021-07-11 23:06:53 +0700

    workshop

---
+++ w01_numeric_oparation/src/main.rs
@@ -0,0 +1,22 @@
+fn main() {
+    // addition
+    let sum = 5 + 10;
+    println!("sum = {}",sum);
+
+    // subtraction
+    let difference = 95.5 - 4.3;
+    println!("difference = {}",difference);
+
+    // multiplication
+    let product = 4 * 30;
+    println!("product = {}",product);
+
+    // division
+    let quotient = 56.7 / 32.2;
+    println!("quotient = {}",quotient);
+
+    // remainder
+    let remainder = 43 % 3;
+    println!("remainder = {}",remainder);
+}
+
